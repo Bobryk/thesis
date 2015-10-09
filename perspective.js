292f4bcb1a5198fd257240683f47b2ba888226e8
Nicholas Bobryk <nicholasbobryk@Skys-Work-Computer.local>
2015-10-09 15:24:51 -0400

    resolved merge issue

--- perspective.js
+++ perspective.js
@@ -2,28 +2,6 @@ document.body.onload = addElements();
 
 
 function addElements(){
-<<<<<<< HEAD
-	var boxes = document.getElementsByClassName("box");
-	for(var i=0; i<boxes.length; i++) {
-		
-		
-		var rightWall = document.createElement("figure");
-			rightWall.className = "right";
-			boxes[i].appendChild(rightWall);
-		var topWall = document.createElement("figure");
-			topWall.className = "top";
-			boxes[i].appendChild(topWall);
-		var leftWall = document.createElement("figure");
-			leftWall.className = "left";
-			boxes[i].appendChild(leftWall);
-		var bottomWall = document.createElement("figure");
-			bottomWall.className = "bottom";
-			boxes[i].appendChild(bottomWall);
-		var frontWall = document.createElement("figure");
-			frontWall.className = "front";
-			boxes[i].appendChild(frontWall);
-		}
-=======
 	var boxes = document.getElementsByClassName("perspective-box");
 	for(var i=0; i<boxes.length; i++) {
 		var inner = document.createElement("div");
@@ -45,7 +23,4 @@ function addElements(){
 
 		boxes[i].insertBefore(inner, boxes[i].firstChild);
 		}
-		
->>>>>>> gh-pages
-
 }
