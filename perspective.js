1e3dc6f0adda6e7a96c081d7c7ae3d4bbd4f8d45
Nicholas Bobryk <nicholasbobryk@Skys-Work-Computer.local>
2015-10-09 11:51:46 -0400

    making boxes relative

--- perspective.js
+++ perspective.js
@@ -4,23 +4,26 @@ document.body.onload = addElements();
 function addElements(){
 	var boxes = document.getElementsByClassName("box");
 	for(var i=0; i<boxes.length; i++) {
-		
+		var inner = document.createElement("div");
+		inner.className = "perspective-box-inner";
+
 		
 		var rightWall = document.createElement("figure");
 			rightWall.className = "right";
-			boxes[i].appendChild(rightWall);
+			inner.appendChild(rightWall);
 		var topWall = document.createElement("figure");
 			topWall.className = "top";
-			boxes[i].appendChild(topWall);
+			inner.appendChild(topWall);
 		var leftWall = document.createElement("figure");
 			leftWall.className = "left";
-			boxes[i].appendChild(leftWall);
+			inner.appendChild(leftWall);
 		var bottomWall = document.createElement("figure");
 			bottomWall.className = "bottom";
-			boxes[i].appendChild(bottomWall);
+			inner.appendChild(bottomWall);
 		var frontWall = document.createElement("figure");
 			frontWall.className = "front";
-			boxes[i].appendChild(frontWall);
+			inner.appendChild(frontWall);
 		}
+		boxes[i].appendChild(inner);
 
 }
