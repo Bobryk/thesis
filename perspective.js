d2cd17e91cff13002a91ca63a140a2f78f21187c
soacuser <soacuser@aimmmisc206-02.lions.tcnj.edu>
2016-03-23 09:39:54 -0400

    padding fixed

--- perspective.js
+++ perspective.js
@@ -10,17 +10,30 @@ function addElements(){
 				inner.style.perspectiveOrigin = "2000px -2000px";
 				inner.style.perspective = "10000px";
 			}
+			if(window.getComputedStyle(boxes[i], null).getPropertyValue('padding-left')!="0px"||window.getComputedStyle(boxes[i], null).getPropertyValue('padding-top')!="0px"){
+				var paddingLeft = window.getComputedStyle(boxes[i], null).getPropertyValue('padding-left');
+				paddingLeft = "-" + paddingLeft;
+				inner.style.marginLeft = paddingLeft;
+				var paddingTop = window.getComputedStyle(boxes[i], null).getPropertyValue('padding-top');
+				paddingTop = "-" + paddingTop;
+				inner.style.marginTop = paddingTop;
+			}
+			inner.style.border = "initial";
 		var rightWall = document.createElement("figure");
 			rightWall.className = "right";
+			rightWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
 			inner.appendChild(rightWall);
 		var topWall = document.createElement("figure");
 			topWall.className = "top";
+			topWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
 			inner.appendChild(topWall);
 		var leftWall = document.createElement("figure");
 			leftWall.className = "left";
+			leftWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
 			inner.appendChild(leftWall);
 		var bottomWall = document.createElement("figure");
 			bottomWall.className = "bottom";
+			bottomWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
 			inner.appendChild(bottomWall);
 		
 
