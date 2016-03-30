33cd2362285318301134cef38d406669ff0dc092
soacuser <soacuser@aimmmisc206-02.lions.tcnj.edu>
2016-03-30 09:30:35 -0400

    a bunch of things

--- perspective.js
+++ perspective.js
@@ -2,43 +2,65 @@ document.body.onload = addElements();
 
 
 function addElements(){
-	var boxes = document.getElementsByClassName("perspective-box");
-	for(var i=0; i<boxes.length; i++) {
-		var inner = document.createElement("div");
+	var depthlyElements = document.getElementsByClassName("depthly");
+	var depth = 0;
+	var isPerspective, isBox, hasDepth = false;
+	for(var i = 0;i<depthlyElements.length;i++){
+		isPerspective, isBox, hasDepth = false;
+		for(var j=0;j<depthlyElements[i].classList.length;j++){
+			if(depthlyElements[i].classList[j]=='perspective')isPerspective=true;
+			if(depthlyElements[i].classList[j]=='orthographic')isPerspective=false;
+			if(depthlyElements[i].classList[j]=='perspective-box')isBox=true;
+			if(depthlyElements[i].classList[j].indexOf('depth-')!=-1){
+				hasDepth=true;
+				depth = parseInt(depthlyElements[i].classList[j].substring(5));
+				depthlyElements[i].style.transform="translateZ("+depth*10+"px)";
+				depthlyElements[i].style.webkitTransform="translateZ("+depth*10+"px)";
+				depthlyElements[i].style.zIndex="-20";
+			}
+		}
+		
+		if(isBox&&!hasDepth){
+			var box = depthlyElements[i];
+			var inner = document.createElement("div");
 			inner.className = "perspective-box-inner";
-			if(typeof InstallTrigger !== 'undefined'){
+			inner.style.zIndex="-21";
+			if(typeof InstallTrigger !== 'undefined'||!isPerspective){
 				inner.style.perspectiveOrigin = "2000px -2000px";
 				inner.style.perspective = "10000px";
 			}
-			if(window.getComputedStyle(boxes[i], null).getPropertyValue('padding-left')!="0px"||window.getComputedStyle(boxes[i], null).getPropertyValue('padding-top')!="0px"){
-				var paddingLeft = window.getComputedStyle(boxes[i], null).getPropertyValue('padding-left');
+			if(window.getComputedStyle(box, null).getPropertyValue('padding-left')!="0px"||window.getComputedStyle(box, null).getPropertyValue('padding-top')!="0px"){
+				var paddingLeft = window.getComputedStyle(box, null).getPropertyValue('padding-left');
 				paddingLeft = "-" + paddingLeft;
 				inner.style.marginLeft = paddingLeft;
-				var paddingTop = window.getComputedStyle(boxes[i], null).getPropertyValue('padding-top');
+				var paddingTop = window.getComputedStyle(box, null).getPropertyValue('padding-top');
 				paddingTop = "-" + paddingTop;
 				inner.style.marginTop = paddingTop;
 			}
 			inner.style.border = "initial";
-		var rightWall = document.createElement("figure");
-			rightWall.className = "right";
-			rightWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
-			inner.appendChild(rightWall);
-		var topWall = document.createElement("figure");
-			topWall.className = "top";
-			topWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
-			inner.appendChild(topWall);
-		var leftWall = document.createElement("figure");
-			leftWall.className = "left";
-			leftWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
-			inner.appendChild(leftWall);
-		var bottomWall = document.createElement("figure");
-			bottomWall.className = "bottom";
-			bottomWall.style.border = window.getComputedStyle(boxes[i], null).getPropertyValue('border');
-			inner.appendChild(bottomWall);
-		
+			var rightWall = document.createElement("figure");
+				rightWall.className = "right";
+				rightWall.style.border = window.getComputedStyle(box, null).getPropertyValue('border');
+				inner.appendChild(rightWall);
+			var topWall = document.createElement("figure");
+				topWall.className = "top";
+				topWall.style.border = window.getComputedStyle(box, null).getPropertyValue('border');
+				inner.appendChild(topWall);
+			var leftWall = document.createElement("figure");
+				leftWall.className = "left";
+				leftWall.style.border = window.getComputedStyle(box, null).getPropertyValue('border');
+				inner.appendChild(leftWall);
+			var bottomWall = document.createElement("figure");
+				bottomWall.className = "bottom";
+				bottomWall.style.border = window.getComputedStyle(box, null).getPropertyValue('border');
+				inner.appendChild(bottomWall);
+			
 
-		boxes[i].insertBefore(inner, boxes[i].firstChild);
+			box.insertBefore(inner, box.firstChild);
+		}
+		
 	}
+	
 	var boxes = document.getElementsByClassName("perspective-fade");
 	for(var j = 0;j<boxes.length;j++){
 		var inner = document.createElement("div");
